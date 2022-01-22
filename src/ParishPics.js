cc97c50e5e6cd4d97ce1e22f38cca3e64d20deaf
Benjamin Parish <parishben13@gmail.com>
2022-01-22 18:44:03 -0500

    Changed ParishPics component to render table of photos rather than UL. Refactored RealisticConfetti to shoot 3 bursts of confetti

--- src/ParishPics.js
+++ src/ParishPics.js
@@ -7,8 +7,11 @@ const ParishPics = () => {
         <div style={{position:'relative', top:'20pt', textAlign:'center', backgroundColor:'darkgoldenrod'}}>
             <Realistic/>
             <p style={{fontWeight:'bolder',color:'white'}}><em> Although '20-'21 has been really trying on so many - it brought us this precious bundle of joy!!</em></p>
-            <ul style={{listStyle: "none"}}><li><img src= "https://live.staticflickr.com/65535/51823489824_ca33c82976.jpg" width="500" height="480" alt="Rory Clause" title="Miss Rory Santa Clause" /></li></ul>
-
+             <table > 
+                <tr>
+                    <td><img src= "https://live.staticflickr.com/65535/51823489824_ca33c82976.jpg" width="500" height="480" alt="Rory Clause" title="Miss Rory Santa Clause" /></td><td><img src= "https://live.staticflickr.com/65535/51823489824_ca33c82976.jpg" width="500" height="480" alt="Rory Clause" title="Miss Rory Santa Clause" /></td><td><img src= "https://live.staticflickr.com/65535/51823489824_ca33c82976.jpg" width="500" height="480" alt="Rory Clause" title="Miss Rory Santa Clause" /></td> 
+                </tr>
+                </table>
 
 
         </div>
--- src/RealisticConfetti.js
+++ src/RealisticConfetti.js
@@ -18,22 +18,41 @@ export default class Realistic extends React.Component {
 
 componentDidMount(){
     this.handlerFire()
+    this.handlerFire2()
+    this.handlerFire3()
 }
 
   makeShot = (particleRatio, opts) => {
      // console.log(this.animationInstance)
     this.animationInstance && this.animationInstance({
       ...opts,
-      origin: { y: 0.7 },
+      origin: {  y: 0.7 },
       particleCount: Math.floor(200 * particleRatio),
     });
   }
+  leftShot = (particleRatio, opts) => {
+    // console.log(this.animationInstance)
+   this.animationInstance && this.animationInstance({
+     ...opts,
+     origin: { x:0.0, y: 0.7 },
+     particleCount: Math.floor(200 * particleRatio),
+   });
+ }
+ rightShot = (particleRatio, opts) => {
+    // console.log(this.animationInstance)
+   this.animationInstance && this.animationInstance({
+     ...opts,
+     origin: { x:1.0, y: 0.7 },
+     particleCount: Math.floor(200 * particleRatio),
+   });
+ }
 
   fire = () => {
      // console.log(this.animationInstance)
     this.makeShot(0.25, {
       spread: 26,
       startVelocity: 55,
+      origin: 'x:0.0'
     });
 
     this.makeShot(0.2, {
@@ -58,10 +77,78 @@ componentDidMount(){
       startVelocity: 45,
     });
   }
+  fireLeft = () => {
+    // console.log(this.animationInstance)
+   this.leftShot(0.25, {
+     spread: 26,
+     startVelocity: 55,
+     origin: 'x:0.0'
+   });
+
+   this.leftShot(0.2, {
+     spread: 60,
+   });
+
+   this.leftShot(0.35, {
+     spread: 100,
+     decay: 0.91,
+     scalar: 0.8,
+   });
+//
+   this.leftShot(0.1, {
+     spread: 120,
+     startVelocity: 25,
+     decay: 0.92,
+     scalar: 1.2,
+   });
+
+   this.leftShot(0.1, {
+     spread: 120,
+     startVelocity: 45,
+   });
+ }
+
+ fireRight = () => {
+    // console.log(this.animationInstance)
+   this.rightShot(0.25, {
+     spread: 26,
+     startVelocity: 55,
+     origin: 'x:0.0'
+   });
+
+   this.rightShot(0.2, {
+     spread: 60,
+   });
+
+   this.rightShot(0.35, {
+     spread: 100,
+     decay: 0.91,
+     scalar: 0.8,
+   });
+
+   this.rightShot(0.1, {
+     spread: 120,
+     startVelocity: 25,
+     decay: 0.92,
+     scalar: 1.2,
+   });
+
+   this.rightShot(0.1, {
+     spread: 120,
+     startVelocity: 45,
+   });
+ }
+
 
   handlerFire = () => {
     this.fire();
   };
+  handlerFire2 = () => {
+      this.fireLeft();
+  }
+  handlerFire3 = () => {
+      this.fireRight();
+  }
 
   getInstance = (instance) => {
     this.animationInstance = instance;
