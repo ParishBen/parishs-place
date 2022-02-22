7608e19fbff8919d1701fa39ddcf441181b53bd3
Benjamin Parish <parishben13@gmail.com>
2022-02-21 23:18:49 -0500

    Added component to hold all tech blogs

---
+++ src/Blogs.js
@@ -0,0 +1,14 @@
+
+const Blogs = () => {
+    return (
+        <div  style={{backgroundColor:'#ad9c3a', position:'relative',top:'20pt'}}>
+            <h2> Will Show all blogs in list (linked to individual Components)</h2>
+            <ul>
+                <li>Blog1</li>
+                <li>Blog2</li>
+            </ul>
+        </div>
+
+    )
+}
+export default Blogs
