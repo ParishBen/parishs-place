3d62a586ed23b45ac4dde4e9323cb996be2d6494
Benjamin Parish <parishben13@gmail.com>
2022-02-21 23:50:57 -0500

    styling positioning

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,9 +1,9 @@
 
 const Blogs = () => {
     return (
-        <div  style={{backgroundColor:'#ad9c3a', position:'relative',top:'20pt'}}>
+        <div  style={{backgroundColor:'#ad9c3a', position:'relative',top:'12pt'}}>
             <h2> Will Show all blogs in list (linked to individual Components)</h2>
-            <ul>
+            <ul style={{listStyle:'none'}}>
                 <li>Blog1</li>
                 <li>Blog2</li>
             </ul>
