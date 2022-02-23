16f58e8300d3d85293c4bd23b850348641fb0ded
Benjamin Parish <parishben13@gmail.com>
2022-02-22 21:05:48 -0500

    Fixed up Navbar styling. Removed traditional NavLink components of BrowserRouter. Using Bootstrap now

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
- import { NavLink } from 'react-router-dom';
+ import { NavLink, Link } from 'react-router-dom';
 
 // transform: 'translateY(-50%) translateX(-50%)',
 //   padding: '20px 40px',
@@ -24,96 +24,29 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-xl navbar-light bg-light" style={{height:'25pt'}}>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style={{height:'25pt'}}>
     <div class="container-fluid" style={{backgroundColor:'floralwhite', boxShadow: '0 0 20px black', cursor: 'pointer',textTransform:'uppercase',textAlign:'center', paddingBottom: '10px', paddingLeft:'3pt' }}>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse show" id="navbarNavAltMarkup">
     <ul class="navbar-nav">
-      {/* <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li> */}
-        {/* <NavLink class="nav-item"
-        style={{ marginRight: '10px', transform:'translateY(-50%) translateX(-50%)' }} 
-        activeStyle={{background: 'darkblue', color: 'white'}}
-        to='/'
-        exact
-      > 
-         Home
-      </NavLink>  */}
-      <NavLink class = "nav-item"
-        
-        style={{ marginRight: '10px' }} 
-        to="/"
-        exact
-      > 
-         Home
-      </NavLink>
-        {/* <li class="nav-item">
-          <a class="nav-link" href="/parish-pics">Parish Pics</a>
-        </li> */}
-      
-      <NavLink class = "nav-item"
-        
-        style={{ marginRight: '10px' }} 
-        to="/parish-pics"
-        exact
-      > 
-         Parish Pics
-      </NavLink>
-
-      {/* <li class="nav-item">
-          <a class="nav-link" href="/projects">Projects</a>
-        </li> */}
-       <NavLink class="nav-item"
-        style={{ marginRight: '10px' }} 
-        activeStyle={{background: 'darkblue', color: 'white'}}
-        to="/projects"
-        exact
-        >
-        Projects 
-      </NavLink> 
-
-      {/* <NavLink class="nav-item"
-        style={{ marginRight: '10px' }} 
-        activeStyle={{background: 'darkblue', color: 'white'}}
-        to="/blogs"
-        exact
-        >
-        Blogs 
-      </NavLink>  */}
-       <li class="nav-item dropdown">
-          {/* <a class="nav-link dropdown-toggle" href="/blogs" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Blogs
-          </a> */}
-          <NavLink class="nav-link dropdown-toggle" href="/blogs" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false"
-        style={{ marginRight: '10px' }} 
-        activeStyle={{background: 'darkblue', color: 'white'}}
-        to="/blogs"
-        exact
-        >
-        Blogs 
-      </NavLink> 
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><NavLink  
-       
-        activeStyle={{background: 'darkblue', color: 'white'}}
-        to="/blogs"
-        exact
-        >Blog1</NavLink></li>
-            <li><NavLink  
-       
-       activeStyle={{background: 'darkblue', color: 'white'}}
-       to="/blogs"
-       exact
-       >Blog2</NavLink></li>
-            <li><NavLink  
-       
-       activeStyle={{background: 'darkblue', color: 'white'}}
-       to="/blogs"
-       exact
-       >Blog1</NavLink></li>
+     
+      <li class="nav-item">
+      <Link class="nav-link" to="/">Home</Link>
+       </li>
+        <li class="nav-item">
+          <Link class="nav-link" to="/parish-pics">Parish Pics</Link>
+        </li>
+        <li class="nav-item">
+            <Link class="nav-link" to="/projects">Projects</Link>
+          </li>
+          <li class="nav-item dropdown">
+            <Link  class="nav-link dropdown-toggle"  id="navbarDarkDropdownMenuLink" role="link" data-bs-toggle="dropdown" aria-expanded="false" to="/blogs">Blogs</Link>
+          <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+            <li><Link class="dropdown-item" to="/blogs">Blogs1</Link></li>
+            <li><Link class="dropdown-item" to="/blogs">Blogs2</Link></li>
+            <li><Link class="dropdown-item" to="/blogs">Blogs3</Link></li>
           </ul>
          </li>
         </ul>
