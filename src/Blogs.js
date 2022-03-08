5eec746dfa3048c74444f77daeaead4ebb95e5c0
Benjamin Parish <parishben13@gmail.com>
2022-03-08 14:13:21 -0500

    Added index views (rendering all blogs)

--- src/Blogs.js
+++ src/Blogs.js
@@ -4,13 +4,16 @@ import Blog3 from "./Blog3"
 
 const Blogs = () => {
     return (
-        <><span style={{backgroundColor:'#ad9c3a', position:'relative',top:'12pt'}}>
+        <>
+        <div class="container-fluid" style={{backgroundColor:'lightseagreen', boxShadow: '0 0 20px black',textAlign:'center', paddingBottom: '10px', paddingLeft:'3pt' }}>
+        <span style={{backgroundColor:'#ad9c3a', position:'relative',top:'12pt'}}>
             <ul style={{listStyle:'none'}}>
                 <li><Blog1/></li>
                 <li><Blog2/></li>
                 <li><Blog3/></li>
             </ul>
-        </span></>
+        </span>
+        </div></>
 
     )
 }
