30ecdffddd60be66eff794b5da365813c9f6512e
Benjamin Parish <parishben13@gmail.com>
2022-03-08 14:15:21 -0500

    Removed slug info from Blog components and replaced with true data

--- src/Blog.js
+++ src/Blog.js
@@ -1,10 +1,13 @@
-
+import { useParams } from "react-router"
 const Blog = ({props}) => {
+    let params = useParams()
+    //let propp = this.props
+    //const {propps} = this.props
     return (
         <div>
             <h2> Generic Blog title</h2>
             <p>Blog's content</p>
-            {console.log(props)}
+            {console.log(params)}
         </div>
 
     )
--- src/Blog2.js
+++ src/Blog2.js
@@ -1,9 +1,12 @@
 
 const Blog2 = () => {
     return (
-        <div  /*style={{backgroundColor:'#ad9c3a', position:'relative',top:'12pt'}}*/>
-            <h2> Blog 2 Title</h2>
-            <p>Blog2's content</p>
+        // <div  /*style={{backgroundColor:'#ad9c3a', position:'relative',top:'12pt'}}*/>
+                    <div class="container-fluid" style={{backgroundColor:'gold', boxShadow: '0 0 20px black',textAlign:'center', paddingBottom: '10px', paddingLeft:'3pt' }}>
+
+            <h2> Sinatra Project - 'Plant Diary Digest'</h2>
+            <p><t/>This project was certainly full of many twists, turns, & 404’s but hey the controllers were sent back to the right path in the end. This project tasks me with setting up a MVC framework using ActiveRecord relations and Sqlite3 database persisting the data of the web application. The project should hold a has_many & belongs_to Model relationship, follow RESTful routes, & a user having full CRUD for only their objects. To put this into action I came up with the idea for a web application to hold a user’s houseplants. This put the user having many plants “to life”. Then I extended this further so that plants can have many logs of how they are doing and when they were watered. This brought into the picture a user having many logs through plants relationship. Lastly, I thought for UI sake there should be a home page where the user can see how other users’ plants are doing, what they’re doing for their plants, & a tips section. This ‘Home Feed’ page would allow a user to click on the user that posted that log or tip & see that user’s account page. Four models, five controllers, and twenty-five views later I had the relationships all working synchronously! That was a huge moment of pride there.
+            <br/><t/> I found the helpers methods determining whether a user was logged in and whether a logged in user was the current user of a view was IMMENSELY helpful (a nod to its very telling name). I found myself plugging those helpers into almost all the requests to provide security of what data was presented to the page. I am certainly happy with the “finished project”, however I know there are many avenues to further build onto this and am eager to continue learning those future tools that could make this a much more dynamic application.</p>
         </div>
 
     )
