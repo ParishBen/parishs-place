bca1b9ae0c5494f60f269148f5cb164e5386cc05
Benjamin Parish <parishben13@gmail.com>
2022-03-08 14:00:53 -0500

    Added Home Component for index element rendering

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    return (
+<header className="App-header">
+        <p>
+          Hey there! Just a heads up this is still a great work in progress. I'm having fun making progress every chance I get.
+            <br/> Keep checking back for new content!<br/>
+            FROM HOME COMPONENT
+        </p>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+
+    
+
+      </header>
+    )
+}
+export default Home
