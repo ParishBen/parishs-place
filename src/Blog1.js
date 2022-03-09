1ab3723ed8c4483f4a4e84a2057631d185840076
Benjamin Parish <parishben13@gmail.com>
2022-03-08 21:40:43 -0500

    Added and positioned buttons at bottom of Blog1. Navigates to previous or next blog

--- src/Blog1.js
+++ src/Blog1.js
@@ -6,6 +6,9 @@ const Blog1 = () => {
             <p>Persistence is pretty much everything when it comes to building development skills. If you’re being honest with yourself -there are going to be countless times when the dreaded bug(s) presses HALT on your progress of your code. It takes a serious amount of both patience and ingenuity to push pass those roadblocks. Sometimes that’s taking four lefts to get going back in the right direction. I’m consistently touched by how helpful the coding community is with blogs, StackOverflow, Reddit, and several other forums answering whatever question pops in mind. At times, it can be tempting to just keep attacking the bug and/or throw a laptop, but I’ve come to find it much more enriching to “reach out” in these moments. You never know what other really helpful tips and explanations you could be missing! The web is ever vast when it comes to the solutions you seek!
                 <br/><t/>As graduation is on the horizon, I’m making it core tenet to stay focused on being resourceful rather than frustrated. More importantly employing the building blocks of resourcefulness: patience, persistence, and a good tinge of optimism. Keeping these in mind in pursuit of solution and answer(s) to whatever the roadblock may be. It’s key for me to remember the answers are out there I just need to keep ‘digging’. To future bugs, challenge accepted!</p>
            </span> 
+           {console.log(window.location.pathname, window.location.href)}
+        
+        <button id="prev" style={{position:'fixed',left:'2pt'}} onClick={()=>window.location.pathname = "/blogs/1"}>Previous Blog</button>&nbsp;<button id="next" style={{position:'fixed',right:"2pt"}} onClick={()=>window.location.pathname = "/blogs/3"}>Next Blog</button>
         </div>
     )
 }
